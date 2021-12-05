63235c9071154fe475efea295d15f1759081b4e0
CoolhanD <abrawa@gmail.com>
2021-12-05 12:47:44 +0200

    third task done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -8,10 +8,8 @@ const ingredients = [
 ];
 
 const itemList = document.querySelector("#ingredients");
-// const listEl = document.createElement("li");
 ingredients.forEach((element) => {
   const listEl = document.createElement("li");
   listEl.append(element);
-
   itemList.appendChild(listEl);
 });
