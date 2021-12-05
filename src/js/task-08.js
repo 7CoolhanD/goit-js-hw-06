d2654d8885ad11426e66e590801f45465369d69c
CoolhanD <abrawa@gmail.com>
2021-12-05 17:34:42 +0200

    task 8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const eventElements = event.currentTarget.elements;
+  const email = eventElements.email.value;
+  const password = eventElements.password.value;
+
+  if (!email || !password) {
+    alert("enter all types");
+  }
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  form.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
