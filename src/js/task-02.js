7a2b9be0fffef8f3b94e613b65d465082f20a082
CoolhanD <abrawa@gmail.com>
2021-12-05 12:04:24 +0200

    second task done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const itemList = document.querySelector("#ingredients");
+// const listEl = document.createElement("li");
+ingredients.forEach((element) => {
+  const listEl = document.createElement("li");
+  listEl.append(element);
+
+  itemList.appendChild(listEl);
+});
