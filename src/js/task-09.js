d41bf1f2d4a19c0332977a0760225d5b1297150b
CoolhanD <abrawa@gmail.com>
2021-12-05 17:53:33 +0200

    nine task ready

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+const refs = {
+  changeColorBtn: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+  colorValue: document.querySelector(".color"),
+};
+
+refs.changeColorBtn.addEventListener("click", changeBodyColor);
+
+function changeBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorValue.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
