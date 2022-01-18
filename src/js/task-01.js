a869b5f4331d754660f6dcf7d2097ed660974618
CoolhanD <abrawa@gmail.com>
2022-01-18 20:48:04 +0200

    clg

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,4 +1,5 @@
 const allItems = document.querySelectorAll(".item");
+console.log(`Number of categories: ${allItems.length}`);
 
 allItems.forEach((element) => {
   console.log(`Category: ${element.querySelector("h2").textContent}`);
