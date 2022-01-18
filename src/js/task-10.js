fd1ca941346812d887153a94017bfab496f2f55d
CoolhanD <abrawa@gmail.com>
2022-01-18 20:11:25 +0200

    delete comments

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,19 +1,3 @@
-// const refs = {
-//   amountValue: document.querySelector("#controls input").value,
-//   creatBtn: document.querySelector("[data-create]"),
-//   destroyBtn: document.querySelector("[data-destroy]"),
-//   container: document.querySelector("#boxes"),
-// };
-// const getAmount = () => createBoxes(refs.amountValue);
-
-// function createBoxes(amount) {
-//   let baseSize = 30;
-
-//   console.log(getAmount.map((amount) => baseSize + getAmount * 10));
-// }
-
-// refs.creatBtn.addEventListener("click", getAmount);
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -30,20 +14,7 @@ function getAmount() {
   const userValue = document.querySelector("#controls input").value;
   createBoxes(userValue);
 }
-// const createBoxes = (amount) => {
-//   amount = userValue;
-//   console.log(userValue);
-//   for (let i = 0; i < amount; i += 1) {
-//     const basicSize = 30;
-//     const biggestSize = basicSize + i * 10;
-//     const box = document.createElement("div");
-//     box.setAttribute(
-//       "style",
-//       `width: ${biggestSize}px; height: ${biggestSize}px; background: ${getRandomHexColor()}`
-//     );
-//     boxes.append(box);
-//   }
-// };
+
 function createBoxes(amount) {
   const basicSize = 30;
   const fragment = document.createDocumentFragment();
